040999e5a7cd56559355960602afeb1ba4aeea99
Arsen <arsen.test.mess@gmail.com>
2024-11-05 11:44:39 +0200

    Added SECRET_KEY and DEBUG to .env, took variables by dotenv

--- MovieApi/settings.py
+++ MovieApi/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wncrrk!uc6pgdhl8lkn-843ya4re2r32qsha(d2&box=$jy^&("
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
